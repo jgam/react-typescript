a16f5f659a67df4d2512e116ad6f5bc5607fc11e
jgam <jgam@alumni.nd.edu>
2020-04-15 14:55:24 +0900

    functional...in type script...

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component, useState } from "react";
 import { createGlobalStyle } from "styled-components";
+import Number from './components/Number';
 
 const App: React.FC = () => {
   //originally
@@ -8,14 +9,14 @@ const App: React.FC = () => {
     counter:number;
   }
 
-  const [number, setNumber] = useState(0);
+  const [counter, setNumber] = useState(0);
 
   return (
     <div className="App">
-      current Num is : {number}
+      current Num is : {counter}
       <div>
-        <button onClick={() => setNumber(number + 1)}>increase</button>
-        <button onClick={() => setNumber(number - 1)}>decrease</button>
+        <button onClick={() => setNumber(counter + 1)}>increase</button>
+        <button onClick={() => setNumber(counter - 1)}>decrease</button>
       </div>
     </div>
   );
---
+++ src/components/Number.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface Props {
+    Number: number
+}
+
+
+const Number = (props: Props) => {
+    return (
+        <div>
+            propsrp
+        </div>
+    )
+}
+
+export default Number
