84995c880e605aafd112590e17a54bc6c208ffc0
jgam <jgam@alumni.nd.edu>
2020-04-15 14:32:11 +0900

    type script counter done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { createGlobalStyle } from "styled-components";
 
-
-function App() {
+const App: React.FC = () => {
   interface IState{
     counter:number;
   }
+
+  const [number, setNumber] = useState(0);
+
   return (
     <div className="App">
-      typescript
+      current Num is : {number}
+      <div>
+        <button onClick={() => setNumber(number + 1)}>increase</button>
+        <button onClick={() => setNumber(number - 1)}>decrease</button>
+      </div>
     </div>
   );
 }
